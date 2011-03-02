43f0eb9bf976631334924a484306e64612b2ee0a
Saulo Pratti <sppratti@gmail.com>
2011-03-01 22:52:11 -0300

    first commit

---
+++ changelog.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="viewport" content="user-scalable=no, width=device-width" />
+<link rel="stylesheet" type="text/css" href="iphone.css" media="screen" />
+<title>Changelog - Ufes Mobile</title>
+</head>
+<body>
+<div id="container">
+  <div id="toolbar">
+    <h1>Changelog</h1>
+    <a href="sobre.php" class="back">Sobre</a></div>
+  <div id="content" class="fullWidth">
+    <div class="aligncenter brasao"><img src="images/brasaoUfes.png" alt="Universidade Federal do Espírito Santo" width="280" height="77"></div>
+    <div class="list" id="changeList">
+      <ul>
+        <li><a href="#"><strong>Ufes Mobile</strong> 1.0.0 beta</a></li>
+      </ul>
+    </div>
+  </div>
+</div>
+</div>
+<script type="text/javascript" src="javascript/jquery.js"></script>
+<script type="text/javascript" src="javascript/iphone.js" charset="utf-8"></script>
+</body>
+</html>
